0e1d42991cd9fb5417a87a2c3cf7a04435b5da1e
CZeringue <cczeringue@icloud.com>
2015-11-17 23:59:40 -0800

    work

--- js/app.js
+++ js/app.js
@@ -3,12 +3,11 @@ $(document).ready(function() {
 
 $('#nav').find('button').on('click', function () {
 	$(this).hide();
-	$('#lightbox').slideToggle();
+	$('#lightbox').slideToggle('slow');
 });
 
 $('#lightbox').find('i').on('click',function() {
-	$('#lightbox').slideToggle();
+	$('#lightbox').slideToggle('slow');
 	$('#nav').find('button').show();
-})
-	
+});	
 });
