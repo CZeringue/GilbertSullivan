20fa631b25428cb03c9256a6cfaf8cb9b7883532
CZeringue <cczeringue@icloud.com>
2015-11-17 16:01:00 -0800

    fixes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
 
+
 $('#nav').find('button').on('click', function () {
-	$(this).remove();
+	$(this).hide();
 	var ul = $("#nav li").slideToggle();
 	var lightbox = '<div id="lightbox">' + '</div>';
 	$('body').append(lightbox);
 	$('#lightbox').append(ul);
 	$(ul).css('text-align', 'center').css('color', 'white').css('list-style','none');
 	$('#lightbox').find('p').on('click', function() {
-		$('#lightbox').remove();
+		$('#lightbox').remove();	
 	});
+
+
 });
 	
 	
