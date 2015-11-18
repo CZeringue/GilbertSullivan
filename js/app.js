830b44061bca181d9ed64cf225755b852c2551b1
CZeringue <cczeringue@icloud.com>
2015-11-17 17:52:49 -0800

    working lightbox

--- js/app.js
+++ js/app.js
@@ -3,19 +3,12 @@ $(document).ready(function() {
 
 $('#nav').find('button').on('click', function () {
 	$(this).hide();
-	var ul = $("#nav li").slideToggle();
-	var lightbox = '<div id="lightbox">' + '</div>';
-	$('body').append(lightbox);
-	$('#lightbox').append(ul);
-	$(ul).css('text-align', 'center').css('color', 'white').css('list-style','none');
-	$('#lightbox').find('p').on('click', function() {
-		$('#lightbox').remove();	
-	});
-
-
-});
-	
-	
+	$('#lightbox').css('display','block');
 });
 
-
+$('#lightbox').find('i').on('click',function() {
+	$('#lightbox').css('display','none');
+	$('#nav').find('button').show();
+})
+	
+});
